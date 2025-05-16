bfee1c5f72aa2a529247ae11897eab82598d2921
Frankincense <frankincensewelsey@gmail.com>
2025-05-16 10:48:09 +0300

    regex bonus

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,15 @@
 import re
 
-my_pattern = r""
+FINDALL_STRING = """
+    It's such a lovely day today.
+    Where'd all the time go?
+    Some weather we're having today, huh?
+    Tomorrow never knows!
+    Maybe today's just not my day.
+    It's clobbering time!
+"""
+
+my_pattern = r"[A-Z][A-z',\s]+today[A-z',\s]+[.?]|[A-Z][A-z',\s]+today\."
 my_regex = re.compile(my_pattern)
+
 
